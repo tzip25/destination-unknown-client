5c1282d6dce3fb6904d1ed0f01471066d406faa2
github username <github email address>
2019-05-09 21:25:05 -0400

    fixed spelling error

--- src/components/RoundTripFlight.js
+++ src/components/RoundTripFlight.js
@@ -19,14 +19,14 @@ const RoundTripFlight = (props) => {
         </thead>
         <tbody>
           <tr>
-            <td rowspan="3" className="flight-price">{currencySymbol()}{props.flight[0].price}</td>
+            <td rowSpan="3" className="flight-price">{currencySymbol()}{props.flight[0].price}</td>
             <td><img src={props.flight[1].airline_logo} width="35" alt="airline logo"/></td>
             <td className="flight-route">{props.flight[1].start_location} ({props.flight[1].start_airport}) → {props.flight[1].end_location} ({props.flight[1].end_airport})
             <br/>
             <span id="small-flight-text">{props.flight[1].airline}</span></td>
             <td>{props.flight[1].departure_time}<br/><span id="small-flight-text">{new Date(`${props.flight[1].departure_date} EDT`).toDateString()}</span></td>
             <td>{props.flight[1].arrival_time}<br/><span id="small-flight-text">{new Date(`${props.flight[1].arrival_date} EDT`).toDateString()}</span></td>
-            {props.button ? <td rowspan="3"><div className="ui right floated button blue" onClick={() => props.handleClick(props.flight)}>Book Flight</div></td> : null}
+            {props.button ? <td rowSpan="3"><div className="ui right floated button blue" onClick={() => props.handleClick(props.flight)}>Book Flight</div></td> : null}
           </tr>
           <tr>
             <td><img src={props.flight[2].airline_logo} width="35" alt="airline logo"/></td>
